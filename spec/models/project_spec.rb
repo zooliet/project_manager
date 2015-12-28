7a923cb40854dea79ff8afb5a8586180c0284fc3
Junhyun Shin <hl1sqi@gmail.com>
2015-12-29 00:44:12 +0900

    fail: marks a project done if its tasks are done

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe Project do
       #expect(project.done?).to be_falsy
       expect(project).not_to be_done
     end
+
+    it "marks a project done if its tasks are done" do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
+    end
   end
 end
