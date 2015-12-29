6785315d20e777e4a1145a48b6eb02ff924e5c1f
Junhyun Shin <hl1sqi@gmail.com>
2015-12-29 12:41:10 +0900

    pass: knows its velocity, rate, time remaining

--- app/models/project.rb
+++ app/models/project.rb
@@ -22,4 +22,16 @@ class Project
   def remaining_size
     incomplete_tasks.sum(&:size)
   end
+
+  def completed_velocity
+    tasks.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0 / 21
+  end
+
+  def projected_days_remaining
+    remaining_size / current_rate
+  end
 end
